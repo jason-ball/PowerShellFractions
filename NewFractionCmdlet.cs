2269c99d2d79a2f021ca0f3499cf415dee9810b5
Jason Ball <jason@jasonball.dev>
2021-12-01 20:40:22 -0800

    Add MIT License
    Signed-off-by: Jason Ball <jason@jasonball.dev>

---
+++ NewFractionCmdlet.cs
@@ -0,0 +1,52 @@
+﻿/* MIT License
+ *
+ * Copyright (c) 2021 Jason Ball
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace PowerShellFractions
+{
+    [Cmdlet(VerbsCommon.New, "Fraction")]
+    [OutputType(typeof(Fraction))]
+    public class NewFractionCmdlet: PSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ValueFromPipelineByPropertyName = true)]
+        public int Numerator { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            Position = 1,
+            ValueFromPipelineByPropertyName = true)]
+        public int Denominator { get; set; }
+
+        // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
+        protected override void ProcessRecord()
+        {
+            WriteObject(new Fraction(Numerator, Denominator));
+        }
+    }
+}
